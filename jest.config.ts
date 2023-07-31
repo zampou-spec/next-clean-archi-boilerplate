c8507170aa9058202eef752deb0b586847b21097
Zampou Ibrahim <zampou@192.168.1.3>
2023-07-31 20:29:06 +0000

    Fix test bug

--- jest.config.ts
+++ jest.config.ts
@@ -1,46 +1,39 @@
-// jest.config.ts
-
 export default {
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
-  collectCoverageFrom: [
-    "<rootDir>/src/**/*.{ts,tsx}",
-    "!<rootDir>/src/main/**/*",
-    "!<rootDir>/src/**/index.ts",
-    "!**/*.d.ts",
-  ],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/main/**/*', '!<rootDir>/src/**/index.ts', '!**/*.d.ts'],
   clearMocks: true,
-  coverageProvider: "v8",
-  setupFilesAfterEnv: ["<rootDir>/src/shared/settings/jest-setup.ts"],
-  testEnvironment: "jest-environment-jsdom",
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  coverageProvider: 'v8',
+  setupFilesAfterEnv: ['<rootDir>/src/shared/settings/jest-setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   moduleNameMapper: {
-    "tests/(.*)": "<rootDir>/tests/$1",
-    "(.*)": "<rootDir>/src/$1",
+    '^/(.*)$': '<rootDir>/src/$1',
+    'tests/(.*)': '<rootDir>/tests/$1'
   },
   transform: {
-    "^.+\\.(t|j)sx?$": [
-      "@swc/jest",
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
       {
         jsc: {
           parser: {
-            syntax: "typescript",
+            syntax: 'typescript',
             tsx: true,
-            decorators: true,
+            decorators: true
           },
           keepClassNames: true,
           transform: {
             legacyDecorator: true,
             decoratorMetadata: true,
             react: {
-              runtime: "automatic",
-            },
-          },
+              runtime: 'automatic'
+            }
+          }
         },
         module: {
-          type: "es6",
-          noInterop: false,
-        },
-      },
-    ],
-  },
+          type: 'es6',
+          noInterop: false
+        }
+      }
+    ]
+  }
 };
