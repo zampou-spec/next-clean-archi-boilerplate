00328fa7ad1584402df5fe092f4bb4554de3b3b6
Zampou Ibrahim <zampou@Zampous-MacBook-Pro.local>
2023-10-03 12:34:26 +0000

    Fix deployement bug 6

--- src/shared/settings/api-client.ts
+++ src/shared/settings/api-client.ts
@@ -56,7 +56,7 @@ const kyDefaultOptions: Options = {
 
 const apiClient = ky
   .create({
-    prefixUrl: 'http://localhost:8000' //'http://127.0.0.1:8000'
+    prefixUrl: process.env.BACKEND_URL || 'https://api-vamos.mlmhipe.com'
   })
   .extend(kyDefaultOptions);
 
